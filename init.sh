63848fb394b129358adc08d331ee12c24c825ab3
Albert_Lee <iamblwb@gmail.com>
2018-10-15 01:26:17 +0800

    init.sh

---
+++ init.sh
@@ -0,0 +1,18 @@
+sudo apt-get -y update && sudo apt-get -y upgrade
+sudo apt-get -y install build-essential
+sudo apt-get -y install aptitude
+
+sudo apt-get -y install libstdc++6
+
+git clone https://github.com/SDChain/SDChain-Core.git
+git checkout `git rev-list -n 1 --before="2018-10-10" master`
+cd SDChain-Core/bin
+tar xvzf sdchaind-v0.3.7.tar.gz
+cd sdchaind-v0.3.7
+
+chmod +x libprotobuf.so.8
+sudo cp libprotobuf.so.8 /usr/lib/x86_64-linux-gnu/libprotobuf.so.8
+chmod +x sdchaind
+cp ../SDChain-core.cfg ./SDChain-core.cfg
+cp ../validators.txt ./validators.txt
+./sdchaind
